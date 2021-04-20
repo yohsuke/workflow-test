c25ec2c97c33d2ab92fcd2f986ea9105eae5c4c7
Yohsuke FURUTA <yohsuke.furuta@exwzd.com>
2021-04-20 18:27:09 +0900

    add file

---
+++ example.py
@@ -0,0 +1,16 @@
+
+def add(a, b):
+    '''
+    >>> add(1, 2)
+    3
+    >>> add(5, 10)
+    maybe 15
+    '''
+
+    return a + b
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
